0ea9298e64fc413a653d1d94161ece95ec94abb3
Jay-Deeee <144668463+Jay-Deeee@users.noreply.github.com>
2024-10-28 14:39:43 +0800

    Added win and lose conditions for both sides

--- script.js
+++ script.js
@@ -26,5 +26,35 @@ function getComputerChoice() {
 
 function getHumanChoice() {
     let choice = window.prompt("Rock, Paper, or Scissors?")
+    console.log(`You play: ${choice}`)
     return choice;
 }
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice.toLowerCase() === computerChoice.toLowerCase()) {
+        console.log("It's a tie!")
+    } else if (humanChoice.toLowerCase() == "rock" && computerChoice.toLowerCase() == "scissors") {
+        humanScore++
+        console.log("You win! Rock beats scissors.")
+    } else if (humanChoice.toLowerCase() == "paper" && computerChoice.toLowerCase() == "rock") {
+        humanScore++
+        console.log("You win! Paper beats rock.")
+    } else if (humanChoice.toLowerCase() == "scissors" && computerChoice.toLowerCase() == "paper") {
+        humanScore++
+        console.log("You win! Scissors beats paper.")
+    } else if (computerChoice.toLowerCase() == "rock" && humanChoice.toLowerCase() == "scissors") {
+        computerScore++
+        console.log("You lose! Rock beats scissors.")
+    } else if (computerChoice.toLowerCase() == "paper" && humanChoice.toLowerCase() == "rock") {
+        computerScore++
+        console.log("You lose! Paper beats rock.")
+    } else if (computerChoice.toLowerCase() == "scissors" && humanChoice.toLowerCase() == "paper") {
+        computerScore++
+        console.log("You lose! Scissors beats paper.")
+    }
+}
+
+function playGame() {
+}
+
+playRound(humanSelection, computerSelection);
